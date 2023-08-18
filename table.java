1916028d0a3ec4c3900dc220bad0896e1c09d8dd
Gurmeet Kaur <131999481+kgurmeet@users.noreply.github.com>
2023-08-18 10:53:12 +0530

    Create table.java

---
+++ table.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class table {
+    public static void main(String[] args) {
+        int n;
+        Scanner sc=new Scanner(System.in);
+        System.out.println("enter any number");
+        n=sc.nextInt();
+        for(int i=1;i<=10;i++)
+        {
+            System.err.println(n*i);
+        }
+    }
+    
+}
